ed1c9b9f4af72e36a7c5625a9cedd1fbd1230e70
rotysz <44545129+rotysz@users.noreply.github.com>
2018-10-30 23:05:21 +0100

    Create custom.ts

---
+++ custom.ts
@@ -0,0 +1,25 @@
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+namespace RobotImp {
+    export function MotorLeft(SpeedVal: number) {
+        if (SpeedVal >= 0) maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, SpeedVal)
+        else maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, -SpeedVal)
+    }
+
+    export function MotorRight(SpeedVal: number) {
+        if (SpeedVal >= 0) maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, SpeedVal)
+        else maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, -SpeedVal)
+    }
+
+    export function GetDistance(): number {
+        return maqueen.sensor(PingUnit.Centimeters);
+    }
+    export function LineSensorStatus(): number {
+        let Line1Sensor = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+        let Line2Sensor = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+        return Line1Sensor + Line2Sensor * 10
+    }
+}
